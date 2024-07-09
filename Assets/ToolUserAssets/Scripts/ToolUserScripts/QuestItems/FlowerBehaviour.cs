1a75a802a89a436184c208662d12f1eff372e0dd
Fasarion <oscara_93@hotmail.com>
2024-07-09 13:16:40 +0200

    Made some tutorial related updates

--- Assets/ToolUserAssets/Scripts/ToolUserScripts/QuestItems/FlowerBehaviour.cs
+++ Assets/ToolUserAssets/Scripts/ToolUserScripts/QuestItems/FlowerBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlowersBehaviour : MonoBehaviour
+public class FlowerBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
